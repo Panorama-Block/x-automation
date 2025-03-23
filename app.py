4a686e200dd59e5048c4bad12497e3de6b3d2f5c
Felipe Saadi <54749257+FelipeSaadi@users.noreply.github.com>
2025-03-22 21:22:49 -0300

    fix: add rate_limit

--- app.py
+++ app.py
@@ -47,7 +47,8 @@ async def auth_v2():
       consumer_key=API_KEY,
       consumer_secret=API_SECRET,
       access_token=ACCESS_TOKEN,
-      access_token_secret=ACCESS_SECRET
+      access_token_secret=ACCESS_SECRET,
+      wait_on_rate_limit=True
     )
     logger.info("Authentication successful!")
     return client
